777dfb60fc40d47ee3e067afff1c4252fce6b702
ssk090 <shivanandasai.38@gmail.com>
2025-05-17 19:09:18 +0530

    video card completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -6,6 +8,12 @@ const Page = () => {
     <main className='wrapper page'>
       <Header title="All videos" subHeader='Public Library' />
       <h1 className='text-4xl font-karla'>Welcome to Loom Clone</h1>
+
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   )
 }
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,13 +1,20 @@
 import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from '@/constants';
 import React from 'react'
 
 const page = async ({ params }: ParamsWithSearch) => {
-    const { id } = await params;
+    // const { id } = await params;
 
     return (
         <div className='wrapper page'>
             <Header subHeader='shivananda@test.com' title='Shiv | JS Mastery' userImg='/assets/images/dummy.jpg' />
-            <h1 className='text-2xl font-karla'>user id {id}</h1>
+            <section className="video-grid">
+
+                {dummyCards.map((card) => (
+                    <VideoCard key={card.id} {...card} />
+                ))}
+            </section>
         </div>
     )
 }
